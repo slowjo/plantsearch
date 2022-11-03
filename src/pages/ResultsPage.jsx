3bf3ec421519ec87de1813289957f903e7985e55
slowjo <fabiansen.s83@gmail.com>
2022-11-03 11:50:38 +0200

    search bar on search page

--- src/pages/ResultsPage.jsx
+++ src/pages/ResultsPage.jsx
@@ -1,8 +1,22 @@
+import { useRef } from 'react';
 import SearchResultsList from "../components/searchresults/SearchResultsList";
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+import { useTheme } from '@mui/material/styles';
+import SearchField from "../components/search/SearchField";
 
 const ResultsPage = () => {
+    const theme = useTheme();
+
+    const inputRef = useRef();
+
     return (
         <div>
+            <Box sx={{ width: '100%', background: theme.palette.primary.light, py: '1em' }}>
+                <Container>
+                    <SearchField inputRef={inputRef} />
+                </Container>
+            </Box>
             <SearchResultsList />
         </div>
     );
